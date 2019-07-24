c0d95f570cf424bbf0b0c2032c2cc3c5170ead61
Kevin PADILLA <kevin.padilla@decathlon.com>
2019-07-24 17:34:14 +0800

    Log Parcel Perform Request Data

--- app/Http/Controllers/TrackParcelController.php
+++ app/Http/Controllers/TrackParcelController.php
@@ -14,6 +14,15 @@ class TrackParcelController extends Controller
 
 	public function store(Request $Request)
 	{
+		$content  = $Request->getContent();
+		$body     = json_decode($content, true);
+		$filename = sprintf('parcel_tracking_%s_%s.json', $body['data']['parcel_id'], date('YmdHis'));
+		$filePath = storage_path($filename);
+		$file     = fopen($filePath, 'w');
+
+		fwrite($file, json_encode($body));
+		fclose($file);
+
 		return response()->json(['message' => 'success']);
 	}
 }
