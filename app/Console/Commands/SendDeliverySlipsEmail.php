1cb77e823d32dfbc7878a676cacc96b6a54398f0
Kevin PADILLA <kevin.padilla@decathlon.com>
2019-07-29 11:49:28 +0800

    Add log

--- app/Console/Commands/SendDeliverySlipsEmail.php
+++ app/Console/Commands/SendDeliverySlipsEmail.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Services\DeliverySlipPDFService;
+use Illuminate\Support\Facades\Log;
 use DateTime;
 
 class SendDeliverySlipsEmail extends Command
@@ -46,9 +47,11 @@ class SendDeliverySlipsEmail extends Command
         date_default_timezone_set('Asia/Manila');
         $currentTime = date("H:i");
 
-        if (in_array($currentTime, [self::FIRST_BATCH, self::SECOND_BATCH])) {
-            $DeliverySlipPDFService = new DeliverySlipPDFService();
-            $DeliverySlipPDFService->sendDeliverySlips();        
-        }
+        if (!in_array($currentTime, [self::FIRST_BATCH, self::SECOND_BATCH])) {
+            Log::info('Delivery Slip Cron Run Skipped.');
+        }   
+
+        $DeliverySlipPDFService = new DeliverySlipPDFService();
+        $DeliverySlipPDFService->sendDeliverySlips();        
     }
 }
